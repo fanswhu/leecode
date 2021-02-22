70437bbfa47903c223f1a5eb6d1a1d3305ec3a1e
jayce.feng <fengwenxuan1@baonengmotor.com>
2021-02-22 20:06:59 +0800

    无用代码删除

--- app/src/main/java/com/example/myapplication/bean/BaseResponseParams.java
+++ app/src/main/java/com/example/myapplication/bean/BaseResponseParams.java
@@ -1,11 +1,5 @@
 package com.example.myapplication.bean;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
 
 public class BaseResponseParams {
 
